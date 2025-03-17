909279b13ef6eb3323fdc5426272c2ae16f38d31
kairos1228 <goldline10019@gmail.com>
2025-03-17 20:40:10 +0900

    First Commit: Add C/C++ files

---
+++ helloworld/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    printf("¾È³ç, world!\n"); // ¾È³çÇÏ´Â °Í
+    return 0;
+}
---
+++ test_c/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int c = 0;
+int d = 100;
+
+int main()
+{
+    int a = 2;
+    int b = 2;
+    int c = a + b;
+
+    printf("%d + %d = %d\n", a, b, c);
+
+    return 0;
+}
