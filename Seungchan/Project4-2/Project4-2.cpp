12da6eeab6e82f1c3c0fcd5110dc9712c551c0cd
kairos1228 <goldline10019@gmail.com>
2025-03-25 00:55:58 +0900

    Apply .gitignore

---
+++ Seungchan/Project4-2/Project4-2.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "listS.h"
+
+int main(void) {
+    int list[MAX] = { 10, 20, 40, 50, 60, 70 };
+    int i, move, size = 6; // size는 리스트에 있는 원소의 개수
+
+    printf("\n삽입 전 선형 리스트 : ");
+    for (i = 0; i < size; i++) printf("%3d ", list[i]);
+    printf("\n원소의 개수 : %d \n", size);
+
+    move = insertElement(list, size, 30);
+
+    printf("\n삽입 후 선형 리스트 : ");
+    for (i = 0; i <= size; i++) printf("%3d ", list[i]);
+    printf("\n원소의 개수 : %d ", ++size);
+    printf("\n자리 이동 횟수 : %d \n", move);
+
+    move = deleteElement(list, size, 30);
+    if (move == size)
+        printf("\n선형 리스트에 원소가 없어서 삭제할 수 없습니다.\n");
+    else {
+        printf("\n삭제 후 선형 리스트 : ");
+        for (i = 0; i < size - 1; i++) printf("%3d ", list[i]);
+        printf("\n원소의 개수 : %d ", --size);
+        printf("\n자리 이동 횟수 : %d \n", move);
+    }
+
+    getchar();
+    return 0;
+}
+// -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// 자료구조 3주차 - 2 (삽입, 삭제 알고리즘)
+/*
+*  삽입을 위한 이동횟수 = n(마지막 원소 인덱스) - k(삽입할 자리 인덱스) + 1
+*  삭제를 위한 이동횟수 = n(마지막 원소 인덱스) - k(삭제할 자리 인덱스)
+*/
