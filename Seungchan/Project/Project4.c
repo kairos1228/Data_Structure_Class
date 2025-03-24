e3db149a30a6211b4588ebba3b28960c1a15f130
kairos1228 <goldline10019@gmail.com>
2025-03-24 22:12:57 +0900

    update VS file

---
+++ Seungchan/Project/Project4.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    double start, end;
+
+    //for 루프 시작 시간
+    start = ((double)clock() / CLOCKS_PER_SEC);
+
+    // for 루프 10억번 반복
+    int sum = 0;
+    for (i = 0; i < 1000000000; i++){
+        sum ++;
+    }
+
+    // for 루프 끝난 시간
+    end = ((double)clock() / CLOCKS_PER_SEC);
+    printf("프로그램 수행 시간 : %lf\n", (end - start));
+    return 0;
+}
--- Seungchan/helloworld/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello, world!\n");
-    printf("¾È³ç, world!\n"); // ¾È³çÇÏ´Â °Í
-    return 0;
-}
